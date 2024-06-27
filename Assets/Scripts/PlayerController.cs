ff7dfa6d02bed534555397bb5bcaff60de7f08da
GParicio <guillermo.paricio01@gmail.com>
2024-06-27 19:54:22 +0200

    Player prefab changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float mouseSensitivity = 100f;
+    float xRotation = 0f;
+    float yRotation = 0f;
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Getting the mouse input
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Rotating the player
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        yRotation += mouseX;
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
     }
 }
